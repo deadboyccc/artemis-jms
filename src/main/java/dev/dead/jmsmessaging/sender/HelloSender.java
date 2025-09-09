243cfe9fbc52d19c0315a5a170426aa4e28db4f5
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-09 21:15:07 +0300

    tweaked schedule rates/delay to see the output slower

--- src/main/java/dev/dead/jmsmessaging/sender/HelloSender.java
+++ src/main/java/dev/dead/jmsmessaging/sender/HelloSender.java
@@ -6,7 +6,6 @@ import dev.dead.jmsmessaging.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +16,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class HelloSender {
     private  final JmsTemplate jmsTemplate;
-    @Scheduled(fixedRate = 3000,initialDelay = 2500)
+
+    @Scheduled(fixedRate = 8000, initialDelay = 3000)
     public void sendMessage(){
         log.debug("Send Message to Queue");
         HelloWorldMessage helloWorldMessage = HelloWorldMessage.builder()
