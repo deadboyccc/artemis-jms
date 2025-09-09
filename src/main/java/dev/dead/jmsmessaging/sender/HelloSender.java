d0555129eaecdba573d860f021b9a631d920eaf1
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-09 21:18:10 +0300

    added a threadSafe atomicInteger to add counting to the msgs

--- src/main/java/dev/dead/jmsmessaging/sender/HelloSender.java
+++ src/main/java/dev/dead/jmsmessaging/sender/HelloSender.java
@@ -10,11 +10,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class HelloSender {
+    private AtomicInteger counter = new AtomicInteger(0);
     private  final JmsTemplate jmsTemplate;
 
     @Scheduled(fixedRate = 8000, initialDelay = 3000)
@@ -22,7 +24,7 @@ public class HelloSender {
         log.debug("Send Message to Queue");
         HelloWorldMessage helloWorldMessage = HelloWorldMessage.builder()
                 .uuid(UUID.randomUUID())
-                .message("Hello World!")
+                .message("Hello World!: [" + counter.incrementAndGet() + "]")
                 .build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE,helloWorldMessage);
